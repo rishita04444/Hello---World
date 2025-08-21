2601dfe2cb99e213bec2596191ab191fd99cff18
Rishita Goswami <goswamirishita04@gmail.com>
2025-08-21 17:52:45 +0530

    feat(JAVA) : add a new message

--- hello2.java
+++ hello2.java
@@ -3,5 +3,7 @@ public class hello2
     public static void main(String[] args) 
     {
         System.out.println("Hello, GitHub from Java....!");
+        System.out.println("Step by step, I'm learning GitHub commits..");
     }
+
 }
