828b2711ef62a745802e7f0f62d920644b5e0b8c
Rishita Goswami <goswamirishita04@gmail.com>
2025-08-21 16:27:25 +0530

    Add Python, C, and JAVA hello programs

---
+++ hello.py
@@ -0,0 +1,6 @@
+# Simple Python program
+def greet(name="Rishita"):
+    print(f"Hello, GitHub from Python! Hi {name} 👋")
+
+if __name__ == "__main__":
+    greet()
---
+++ hello1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, GitHub from C ...!");
+    return 0;
+}
